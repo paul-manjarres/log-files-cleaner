33b8d294053f2e2e3a52c54a22239b506592e41b
Jean Paul Manjarres Correal <paul.manjarres@gmail.com>
2017-08-14 13:59:22 -0500

    Added commons-cli 1.4 and a very basic PoC

--- src/main/kotlin/org/yagamipaul/tools/logfilecleaner/LogCleanerApp.kt
+++ src/main/kotlin/org/yagamipaul/tools/logfilecleaner/LogCleanerApp.kt
@@ -16,9 +16,7 @@ class LogCleanerApp {
     }
 
 
-    fun start() : ProcessResults{
 
-    }
 
 
 }
--- src/main/kotlin/org/yagamipaul/tools/logfilecleaner/LogCleanerCLI.kt
+++ src/main/kotlin/org/yagamipaul/tools/logfilecleaner/LogCleanerCLI.kt
@@ -1,13 +1,48 @@
 package org.yagamipaul.tools.logfilecleaner
 
+import org.apache.commons.cli.*
+
 /**
+ * Main method.
+ * Used to process the command line arguments.
  *
+ * @author Jean Paul Manjarres Correal
+ * 
  */
-
-
 fun main(args: Array<String>) {
 
     println("Log File Cleaner")
+
+
+    val options : Options = Options()
+
+    val fileOption : Option = Option.builder("f")
+            .hasArg()
+            .longOpt("file")
+            .desc("The file to process")
+            .required()
+            .build()
+
+    val stringsOption : Option = Option.builder("sf")
+            .hasArg()
+            .longOpt("strings")
+            .desc("a path to a file containing the strings to match")
+            .required()
+            .build()
+
+
+    options.addOption(fileOption)
+    options.addOption(stringsOption)
+
+    val parser : CommandLineParser = DefaultParser()
+    val commandLine : CommandLine = parser.parse(options, args)
+
+
+    val filePath = commandLine.getOptionValue("f")
+    val stringsPath = commandLine.getOptionValue("sf")
+
+    println("File path : $filePath")
+    println("Strings path : $stringsPath")
 
 
 }
